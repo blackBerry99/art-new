40526d6122a3fda7492a493308181839cf0ec371
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-12-26 12:33:10 +0200

    reset all

--- assets/compare.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	const twentytwenty = () => {
-		const twentytwentySlider = $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.5});
-	}
-	twentytwenty();
-	document.addEventListener('shopify:section:load', function () {
-		twentytwenty();
-	});
-})();
